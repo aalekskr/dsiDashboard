af8a324228697e40f26dccf4bf3c90e59e181300
Alex K. <alexander.krieger@gmx.net>
2026-03-17 23:08:38 +0100

    First Commit, implemented project structure, structure of main function and a http get function.

---
+++ source/main.c
@@ -0,0 +1,118 @@
+#include <nds.h>
+#include <dswifi9.h>
+#include <netinet/in.h>
+#include <sys/socket.h>
+#include <netdb.h>
+#include <stdio.h>
+#include <sdtlib.h>
+#include <string.h>
+#include <time.h>
+
+//The HomeAssistant config is currently baked into the code while compiling
+//Make sure to change the 3 Variables according to your HA config
+#define HA_HOST_IP "192.168.0.0"
+#define HA_HOST_PORT 8123
+#define HA_LLA_TOKEN "Your long lived access Token"
+
+/*Make sure to define *your own* HA entities. 
+You can have up to 8 Entities displayed*/
+#define ENTITIES_NUM 5
+static const char *ENTITIES_IDS[ENTITIES_NUM] = {
+    "light.kajplats_e14_ws_globe_806lm",
+    "switch.schreibtischlampe",
+    "switch.leselampe",
+    "sensor.eingangstuere",
+    "sensor.wohnzimmer_temperature"
+}
+
+//For the weather entity to work, find out *your own* home weater Entity
+#define WEATHER_ENTITY "weather.forecast_home"
+
+#define SCREEN_X 256
+#define SCREEN_Y 192
+#define MAX_RESP 4096
+
+typedef struct {
+    char entity_id[64];
+    char state[32];
+    char entity_name[64];
+} HAEntity;
+
+static HAEntity entities[ENTITIES_NUM];
+static char weather_state[32] = "n/a";
+static char weather_temperature[16] = "n/a";
+
+static PrintConsole topScreen;
+static PrintConsole bottomScreen;
+
+//HTTP GET function, returns bytes read. Writes into buf.
+static int http_get(const char *host, int port, const char *path, const char *token, char *buf, int bufsz){
+    struct hostent *he = gethostbyname(host);
+    if (!he) return -1;
+    int sock = socket(AF_INET, SOCK_STREAM, 0);
+    if (sock < 0) return -1
+
+    struct sockaddr_in addr;
+    memset(&addr, 0, sizeof(addr));
+    addr.sin_family = AF_INET;
+    addr.sin_port = htons(port);
+    memcpy(&addr.sin_addr, he->h_addr_list[0], he->h_length);
+
+    if (connect(sock, (struct sockaddr *)&addr, sizeof(addr)) < 0){
+        closesocket(sock);
+        return -1;
+    }
+    char req[512];
+    snprintf(req, sizeof(req),
+        "GET %s HTTP/1.0\r\n"
+        "Host: %s:%d\r\n"
+        "Authorization: Bearer %s\r\n"
+        "Content-Type: application/json\r\n"
+        "\r\n", path, host, port, token);
+    send(sock, req, strlen(req), 0);
+
+    int total = 0, n;
+    while (total < bufsz - 1 && (n = recv(sock, buf + total, bufsz -1 - total, 0)) > 0){
+        total += n;
+    }
+    buf[total] = '\0';
+    closesocket(sock);
+    return total;
+}
+
+static void fetch_entity(int enID){
+    //implement
+}
+
+static void fetch_weather(void){
+    //implement
+}
+
+static void drawScreen(void){
+    //implement function to print out the fetched data to the screens
+}
+
+static void setup_consoles(void){
+    //implement
+}
+
+static bool wifi_connected(void){
+    //implement
+}
+
+int main(void){
+    setup_consoles(); //Initialize print consoles. To be implemented.
+    consoleSelect(&topScreen);
+
+    iprintf("Connecting to Wifi..\n");
+    //implement wifi function
+
+    //fetch initial Information
+    //implement fetch_entity and fetch_weather function
+
+    //main loop
+    while(1){
+        //implement draw function for both Screens
+        //implement manual and automatic refresh
+    }
+}
